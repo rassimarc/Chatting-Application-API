1d7593e76ee7a736439f2b3d521d072958116054
Shoushy Kojayan <shoushy.info@gmail.com>
2023-03-27 16:48:46 +0300

    test

--- ProfileService.Web/Dtos/SendMessageResponse.cs
+++ ProfileService.Web/Dtos/SendMessageResponse.cs
@@ -1,8 +1,9 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Cosmos.Serialization.HybridRow;
 
 namespace ProfileService.Web.Dtos;
 
 public record SendMessageResponse(
-    UnixDateTime time
+    [Required] UnixDateTime time
 );
